cbfd335f574d7f3b00f5197f413edaf12f6cbeda
Panji Iman Baskoro <panjidia995@gmail.com>
2020-10-30 12:13:53 +0700

    something

---
+++ main/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="../assets/bulma/css/bulma.min.css">
+  </head>
+  <body>
+  <section class="hero is-primary is-fullheight">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns">
+                <div class="column"></div>
+                <div class="column is-four-fifths"> 
+                    <div class="card">
+                        <div class="card-content">
+                          <h1 class="title has-text-dark">
+                            Selamat datang di Leres. !!
+                            </h1>
+                            <h2 class="subtitle has-text-dark">
+                            Sebelum mulai, yuk pemanasan dulu!. Di bagian kali ini teman-teman akan diajak untuk mengenali berita hoax dengan teknik paling dasar.
+                            </h2>
+                          </br>
+                          <div class="columns">
+                              <div class="column"></div>
+                              <div class="column"><button class="button is-medium is-info is-center is-fullwidth"><strong>Mulai >></strong></button></div>
+                              <div class="column"></div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column"></div>
+            </div>
+        </div>
+    </div>
+  </section>
+  </body>
+</html>

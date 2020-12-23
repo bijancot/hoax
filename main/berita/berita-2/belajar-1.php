46e6a368d1a1834da217656076a7f42bf53d87f3
Panji Iman Baskoro <panjidia995@gmail.com>
2020-12-24 02:24:10 +0700

    update delete done page

--- main/berita/berita-2/belajar-1.php
+++ main/berita/berita-2/belajar-1.php
@@ -23,7 +23,7 @@ $_SESSION['uname'] = $namaTeam;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello Bulma!</title>
-    <link rel="stylesheet" href="../assets/bulma/css/bulma.min.css">
+    <link rel="stylesheet" href="../../../assets/bulma/css/bulma.min.css">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/bijancot/deliv@16/hoax/step.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>

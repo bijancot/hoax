09b0f0de4676df0ee34c88b689b9195e6eb9f018
Panji Iman Baskoro <panjidia995@gmail.com>
2020-12-25 19:50:00 +0700

    fix css call, wrong file

--- main/kuis/index.php
+++ main/kuis/index.php
@@ -21,7 +21,7 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello Bulma!</title>
-    <link rel="stylesheet" href="../../../assets/bulma/css/bulma.min.css">
+    <link rel="stylesheet" href="../../../build/assets/bulma/css/bulma.min.css">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/bijancot/deliv@16/hoax/step.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>

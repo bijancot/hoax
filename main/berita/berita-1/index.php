45537474df607d32ef695229a64bfbbdea572d74
Panji Iman Baskoro <panjidia995@gmail.com>
2020-12-25 19:47:19 +0700

    fix css call, next will be changed using cdn

--- main/berita/berita-1/index.php
+++ main/berita/berita-1/index.php
@@ -10,7 +10,7 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello Bulma!</title>
-    <link rel="stylesheet" href="../../../assets/bulma/css/bulma.min.css">
+    <link rel="stylesheet" href="../../../build/assets/bulma/css/bulma.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
   </head>
   <body>

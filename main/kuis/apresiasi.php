2a90a891cf926c1fd1be12b85113ea4aaa793c8b
Panji Iman Baskoro <panjidia995@gmail.com>
2021-01-17 01:48:13 +0700

    test og reader

--- main/kuis/apresiasi.php
+++ main/kuis/apresiasi.php
@@ -23,7 +23,7 @@ require_once('og.php');
     echo $dataKon."asd";
 
     $graph = OpenGraph::fetch('https://oto.detik.com/catatan-pengendara-mobil/d-5336846/gaib-mobil-tesla-bisa-melihat-hantu-di-kuburan?tag_from=wp_beritautama');    
-    //var_dump($graph->keys());
+    var_dump($graph->keys());
     //var_dump($graph->schema);
     foreach ($graph as $key) {
         echo $key;
